cb5242af8a40bf75f4295939aa2de2394decb232
hbrooker97 <42081142+hbrooker97@users.noreply.github.com>
2020-03-16 23:58:46 +0800

    firstPattern

--- conway.js
+++ conway.js
@@ -1,23 +1,113 @@
 var ctx = document.querySelector("canvas").getContext("2d");
 var numberOfColumns = 200;
-var numberOfRows = 200;
+var gridSize = 250;
 
-var grid = new Array(numberOfRows).fill(0);
+// Initialise the grid
+var grid = new Array(gridSize).fill(0);
 for (var i = 0; i < grid.length; i++){
-    grid[i] = new Array(numberOfColumns).fill(0);
+    grid[i] = new Array(gridSize).fill(0);
 }
+
+// Initialise the grid
+var neighbourStore = new Array(gridSize).fill(0);
+for (var i = 0; i < neighbourStore.length; i++){
+    neighbourStore[i] = new Array(gridSize).fill(0);
+}
+
+
 // Setup the GOL grid
 function initialiseGOL(){
-
+    // Set starting pattern
+    grid[8][18] = 1;
+    grid[8][19] = 1;
+    grid[9][18] = 1;
+    grid[9][19] = 1;
+    grid[18][18] = 1;
+    grid[18][19] = 1;
+    grid[18][20] = 1;
+    grid[19][17] = 1;
+    grid[19][21] = 1;
+    grid[20][16] = 1;
+    grid[20][22] = 1;
+    grid[21][16] = 1;
+    grid[21][22] = 1;
+    grid[22][19] = 1;
+    grid[23][17] = 1;
+    grid[23][21] = 1;
+    grid[24][18] = 1;
+    grid[24][19] = 1;
+    grid[24][20] = 1;
+    grid[25][19] = 1;
+    grid[28][16] = 1;
+    grid[28][17] = 1;
+    grid[28][18] = 1;
+    grid[29][16] = 1;
+    grid[29][17] = 1;
+    grid[29][18] = 1;
+    grid[30][15] = 1;
+    grid[30][19] = 1;
+    grid[32][14] = 1;
+    grid[32][15] = 1;
+    grid[32][19] = 1;
+    grid[32][20] = 1;
+    grid[42][16] = 1;
+    grid[42][17] = 1;
+    grid[43][16] = 1;
+    grid[43][17] = 1;
 }
 
-function getNeighbours(){
+initialiseGOL();
 
+// Get the number of neighbours of a given cell. Wrap grid
+function getNeighbours(i, j){
+    var neighbours = 0;
+    var iBehind = i - 1;
+    var iInfront = i + 1;
+    var jBehind = j - 1;
+    var jInfront = j + 1;
+    if (i == 0){
+        iBehind = gridSize - 1;
+    }
+    if (i == gridSize - 1){
+        iInfront = gridSize - 1;
+    }
+    if (j == 0){
+        jBehind = gridSize - 1;
+    }
+    if (j == gridSize - 1){
+        jInfront = gridSize - 1;
+    }
+    neighbours += grid[iBehind][jBehind];
+    neighbours += grid[iBehind][j];
+    neighbours += grid[iBehind][jInfront];
+    neighbours += grid[i][jBehind];
+    neighbours += grid[i][jInfront];
+    neighbours += grid[iInfront][jBehind];
+    neighbours += grid[iInfront][j];
+    neighbours += grid[iInfront][jInfront];
+    return neighbours;
 }
 
 // Iterate the GOL grid
 function stepGOL(){
-
+    for (var i = 0; i < grid.length; i++){
+        for (var j = 0; j < grid[0].length; j++){
+            neighbourStore[i][j] = getNeighbours(i, j); 
+        }
+    }
+    for (var i = 0; i < grid.length; i++){
+        for (var j = 0; j < grid[0].length; j++){
+            if (neighbourStore[i][j] == 3){
+                grid[i][j] = 1;
+            }
+            if (neighbourStore[i][j] < 2){
+                grid[i][j] = 0;
+            }
+            if (neighbourStore[i][j] > 3){
+                grid[i][j] = 0;
+            } 
+        }
+    }
 }
 
 function resize(canvas) {
@@ -47,10 +137,10 @@ function render() {
     ctx.fillStyle = grd;
     
     ctx.translate(hw, hh);
-    for (var i = 0; i < (numberOfColumns * numberOfRows); ++i) {
+    for (var i = 0; i < (numberOfRows); ++i) {
         hh = h / 2;
-        for (var j = 0; j < numberOfRows; ++j) {
-            if (j % 20 == 0 && i % Math.floor(Math.random() * 20) == 0){
+        for (var j = 0; j < numberOfColumns; ++j) {
+            if (grid[i][j] == 1) {
                 ctx.fillRect(-hw, -hh, cellSize, cellSize);
             }
             hh -= cellSize;
